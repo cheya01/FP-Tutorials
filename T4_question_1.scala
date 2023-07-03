1e47855e3546be976820f6d8cc96e7b2f3399c50
cheyawanigs <chethiya.wanigarathne@gmail.com>
2023-07-04 00:05:35 +0530

    FP tutorial 4

---
+++ T4_question_1.scala
@@ -0,0 +1,27 @@
+package FP_tutorials.FP-Tutorials
+
+object T4_question_1 extends App {
+  println("Enter the deposit amount: ")
+  val dep = scala.io.StdIn.readInt()
+
+  var depInterest: Double = 0
+
+  def calcInterest(dep: Double): Double = {
+    if (dep <= 20000) {
+      depInterest = dep * 0.02
+      return depInterest
+    } else if (dep <= 200000) {
+      depInterest = dep * 0.04
+      return depInterest
+    } else if (dep <= 2000000) {
+      depInterest = dep * 0.035
+      return depInterest
+    } else {
+      depInterest = dep * 0.065
+      return depInterest
+    }
+
+  }
+
+  println("The interest for the deposit is Rs." + calcInterest(dep: Double))
+}
---
+++ T4_question_3.scala
@@ -0,0 +1,30 @@
+package FP_tutorials.FP-Tutorials
+
+object T4_question_3 extends App {
+  def toUpper(name: String): String = {
+    name.toUpperCase()
+  }
+
+  def toLower(name: String): String = {
+    name.toLowerCase()
+  }
+
+  def formatNames(name: String)(formatFunc: String => String): String = {
+    formatFunc(name)
+  }
+
+  val name1 = "Benny"
+  val name2 = "Niroshan"
+  val name3 = "Saman"
+  val name4 = "Kumara"
+
+  val formattedName1 = T4_question_3.formatNames(name1)(T4_question_3.toUpper)
+  val formattedName2 = T4_question_3.formatNames(name2.substring(0, 2))(T4_question_3.toUpper) + name2.substring(2)
+  val formattedName3 = T4_question_3.formatNames(name3)(T4_question_3.toLower)
+  val formattedName4 = T4_question_3.formatNames(name4.substring(0, 1))(T4_question_3.toUpper) + T4_question_3.formatNames(name4.substring(1, 5))(T4_question_3.toLower) + T4_question_3.formatNames(name4.substring(5))(T4_question_3.toUpper)
+
+  println(formattedName1)
+  println(formattedName2)
+  println(formattedName3)
+  println(formattedName4)
+}
